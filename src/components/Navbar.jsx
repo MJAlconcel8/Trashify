15cad4773d6bca025deb2a4a1e67085be02ffd28
Mark Joseph Alconcel <109438699+MJAlconcel8@users.noreply.github.com>
2024-02-24 15:52:18 -0500

    finished navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import logo from '../assets/image-removebg-preview (1) 1.png'
+import logout from '../assets/log-in.svg'
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+        <div className="elementsInNavbar">
+            <img src={logo} alt="" className="trashify" />
+            <div className="navbarPages">
+                <span className='page'>RECYCLING LOCATION</span>
+                <span className='page'>COMMUNITY</span>
+                <span className='page'>STORES</span>
+                <span className='page'>WHO WE ARE</span>
+            </div>
+            <img src={logout} alt="" className='logout'/>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar

c94d2512d3f186451731f97685ef59dd96aa8330
Steeve-Johan Otoka-Eyota <73400373+johanotoka@users.noreply.github.com>
2024-02-24 17:51:01 -0500

    added list of locations

---
+++ src/components/locations/locations.js
@@ -0,0 +1,20 @@
+export const locations = [
+    { "name": "Tec-Com Recycle", "address": "264 Queens Quay W #402, Toronto, ON M5J 1B5" },
+    { "name": "Recycling Partners", "address": "720 King St W #125, Toronto, ON M5V 3S5" },
+    { "name": "Recycle America", "address": "130 King St W, Toronto, ON M5X 2A2" },
+    { "name": "lalarecycle", "address": "27 Queen St E #205, Toronto, ON M5C 1R6" },
+    { "name": "Commissioners Street Transfer Station", "address": "400 Commissioners St, Toronto, ON M4M 3K2" },
+    { "name": "Fine Recycling & Disposal Ltd.", "address": "400 Eastern Ave Ste 202, Toronto, ON M4M 1B9" },
+    { "name": "Viking Recycling", "address": "500 Keele St #408, Toronto, ON M6N 3C9" },
+    { "name": "A B P Recycling Inc.", "address": "100 Glen Scarlett Rd, Toronto, ON M6N 1P4" },
+    { "name": "Independent Recycling", "address": "330 Marwood Cr, Toronto, ON" },
+    { "name": "ASK US GROUP", "address": "1000 Danforth Ave, Toronto, ON M4J 1M2" },
+    { "name": "Recycling Solutions Inc", "address": "123 Eastside Drive, Etobicoke, ON M8Z 5S5" },
+    { "name": "Quantum Lifecycle Partners LP - Toronto", "address": "700 Ormont Dr, North York, ON M9L 2V4" },
+    { "name": "Waste Diversion - Recycling Services", "address": "3800 Victoria Park Ave, North York, ON M2H 3H7" },
+    { "name": "Victoria Park Transfer Station", "address": "3350 Victoria Park Ave, North York, ON M2H 2E1" },
+    { "name": "Manville Recycling", "address": "107 Manville Rd, Scarborough, ON M1L 4J2" },
+    { "name": "Triune Metal Recycling", "address": "787 Warden Ave. Unit 8, Scarborough, ON M1L 4C2" },
+    { "name": "Electronic Recycling Association", "address": "489 Brimley Rd #15, Scarborough, ON M1J 1A3" },
+    { "name": "Delta Recycling", "address": "491 Brimley Rd, Scarborough, ON M1J 1A4" }
+]
